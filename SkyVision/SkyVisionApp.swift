db78cae6faddbad2953a8e27dbdebf12656cc568
Justin Edmund <justin@jedmund.com>
2024-02-19 11:47:38 -0500

    Initial Commit

---
+++ SkyVision/SkyVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyVisionApp.swift
+//  SkyVision
+//
+//  Created by Justin Edmund on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SkyVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
